a8057e65ec8bb3e5d80e57a5e179eddc0216a7f0
Joshua Acosta <Acosta.joshua03@gmail.com>
2018-01-02 19:21:56 -0500

    add config.py file

---
+++ config.py
@@ -0,0 +1,13 @@
+class Config(object):
+    DEBUG = False
+    TESTING = False
+
+
+class ProductionConfig(Config):
+    DATABASE_URI = 'mysql://user@localhost/foo'
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
